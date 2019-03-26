6b98bf7758d000f9099d3779ddf394616a62c8fd
Erin Harrington <erin.c.harrington-1@ou.edu>
2019-03-26 12:31:15 -0500

    Finished GamePieceAppearance

---
+++ src/Color.java
@@ -0,0 +1,33 @@
+
+
+public enum Color {
+
+	RED,
+	GREEN,
+	BLUE,
+	YELLOW,
+	CYAN,
+	MAGENTA;
+	
+	private int r;
+	private int g;
+	private int b;
+	
+	private Color(int r, int g, int b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+	
+	public int getR() {
+		return r;
+	}
+	
+	public int getG() {
+		return g;
+	}
+	
+	public int getB() {
+		return b;
+	}
+}
---
+++ src/Driver.java
@@ -0,0 +1,8 @@
+
+
+public class Driver {
+
+	public void main(String args[]) {
+		
+	}
+}
